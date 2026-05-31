ac5c8f9b822503da4119058a663a66315a15c3cc
bugless-raheem <raheemcoding101@gmail.com>
2026-05-30 21:39:12 -1100

    Latest updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,26 @@
 import { getProducts } from "../data/products";
-import ProductCard from "../components/productcard.jsx";
+import ProductCard from "../components/ProductCard";
 
 export default function Home() {
   const products = getProducts();
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="flex flex-col justify-center items-center text-center">
-        <h1 className="md:text-4xl lg:text-5xl text-2xl  font-bold text-gray-800">
+        <h1 className="md:text-4xl lg:text-5xl text-2xl font-bold text-gray-800">
           Welcome to Shophub
         </h1>
-        <p className="text-gray-600  mt-3 md:text-xl">
+        <p className="text-gray-600 mt-3 md:text-xl">
           Your modern shopping experience starts here
         </p>
       </div>
+
       <div className="">
-        <div className="mt-10 font-bold text-lg md:text-xl text-gray-800 ">
+        <div className="mt-10 font-bold text-lg md:text-xl text-gray-800">
           Our Products
         </div>
-        {/* products grid  */}
+
+        {/* Products Grid */}
         <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-8 mt-8">
           {products.map((product) => (
             <ProductCard product={product} key={product.id} />
